01dd4b0b1cc17845636b0d4140549ed0bca39e69
jerinjerinjerin <133647407+jerinjerinjerin@users.noreply.github.com>
2024-09-28 12:11:11 +0530

    Update Social.jsx

--- components/Social.jsx
+++ components/Social.jsx
@@ -11,7 +11,7 @@ const socials = [
     {icon: <FaGithub/>, path:"https://github.com/jerinjerinjerin"},
     {icon: <FaLinkedinIn/>, path:"https://www.linkedin.com/in/jerin-raj-baa691272/"},
     {icon: <FaWhatsapp/>, path:"https://api.whatsapp.com/send?phone=%2B918015752367"},
-    {icon: <FaInstagram/>, path:""},
+    {icon: <FaInstagram/>, path:"https://www.instagram.com/jerin_js_77/"},
 ];
 
 const Social = ({containerStyle, iconStyles}) => {
