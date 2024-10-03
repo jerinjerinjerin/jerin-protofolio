55d04001111ab1ab7192fee8b503abc9a87a76d4
jerinjerinjerin <133647407+jerinjerinjerin@users.noreply.github.com>
2024-10-03 16:34:28 +0530

    Update layout.jsx

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,9 +1,1 @@
-import React from "react";
 
-const LayOut = ({ children }) => {
-    return (
-        <div className="flex justify-center items-center bg-black">{children}</div>
-    );
-};
-
-export default LayOut;
