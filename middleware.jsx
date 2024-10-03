88539c9104493455ae77b748f549c5d2eb5a1478
jerinjerinjerin <133647407+jerinjerinjerin@users.noreply.github.com>
2024-10-03 16:16:27 +0530

    Update middleware.jsx

--- middleware.jsx
+++ middleware.jsx
@@ -1,20 +1,20 @@
-import {
-  clerkMiddleware,
-  createRouteMatcher
-} from '@clerk/nextjs/server';
+// import {
+//   clerkMiddleware,
+//   createRouteMatcher
+// } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-  '/',
-  '/contact',
-  '/resume',
-  '/services',
-  '/work'
-]);
+// const isProtectedRoute = createRouteMatcher([
+//   '/',
+//   '/contact',
+//   '/resume',
+//   '/services',
+//   '/work'
+// ]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-});
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) auth().protect();
+// });
 
-export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
