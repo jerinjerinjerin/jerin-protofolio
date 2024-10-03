288c716fd01235f23fcd8acdd0d8016acbb987e8
jerinjerinjerin <133647407+jerinjerinjerin@users.noreply.github.com>
2024-10-03 16:35:07 +0530

    Update page.jsx

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,12 +1,1 @@
-"use client"
-import { SignUp } from "@clerk/nextjs";
 
-import Layout from "../../layout";
-
-export default function Page() {
-  return(  
-    <Layout>
-      <SignUp /> 
-    </Layout>
-)
-}
