481d183f0103d9eab46a0c6b1738976bb38e9206
jerinjerinjerin <133647407+jerinjerinjerin@users.noreply.github.com>
2024-10-03 16:31:07 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,26 +18,13 @@ const Header = () => {
             Jerin <span className="text-blue-700">.</span>
           </h1>
         </Link>
-{/*         <div className="flex xl:hidden">
-          <ClerkLoaded>
-            <UserButton afterSignOutUrl="/" />
-          </ClerkLoaded>
-          <ClerkLoading>
-            <Loader2 className="animate-spin size-8 text-slate-400" />
-          </ClerkLoading>
-        </div> */}
-        {/* desktop nav */}
+
         <div className="hidden xl:flex items-center gap-8">
           <Nav />
           <Link href="/contact">
             <Button>Hire Me</Button>
           </Link>
-          <ClerkLoaded>
-            <UserButton afterSignOutUrl="/" />
-          </ClerkLoaded>
-          <ClerkLoading>
-            <Loader2 className="animate-spin size-8 text-slate-400" />
-          </ClerkLoading>
+         
         </div>
         {/* mobile nav */}
         <div className="xl:hidden">
